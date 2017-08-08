0baee18654aab34a6b91c5f7a58b2c2850127d37
meimei1235 <190512054@qq.com>
2017-08-08 15:58:52 +0800

    initial commit

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 function test(num) {
     var regex = new RegExp(/^[0-9]*[1-9][0-9]*$/);
     var i = 2;
-    // var handle = num;
+    var handle = num;
     var arr = [];
-    while (i < num) {     //i小于num则一直循环下去，直到i=num为止
-        var result = num / i;
+    while (i < handle) {     //i小于num则一直循环下去，直到i=num为止
+        var result = handle / i;
         if (regex.test(result)) { //除2结果为整数
             arr.push(i);
             i = 2;
-            num = result;
+            handle = result;
         } else {
             i++;
         }
-        if (i == num) {  //判断是否要结尾
-            arr.push(num)
+        if (i == handle) {  //判断是否要结尾
+            arr.push(handle)
             break;
         }
     }
